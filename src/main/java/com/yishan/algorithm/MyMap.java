3efbd0b20458db2eb20341ad1c3b4d7899d7ee6f
Yishan Xu <yxu1234@umd.edu>
2019-01-18 10:51:34 -0500

    some facts 2

--- src/main/java/com/yishan/algorithm/MyMap.java
+++ src/main/java/com/yishan/algorithm/MyMap.java
@@ -2,15 +2,20 @@ package com.yishan.algorithm;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class MyMap{
 
         public static void main(String[] args) {
             Map<String, String> map = new HashMap<String, String>();
             map.put("The best smart phone is", "iPhone!");
             map.put("The trash smart phones are", "Android phones");
-            for (String key : map.keySet()) {
-                System.out.println(key + " = " + map.get(key));
+            for(Map.Entry<String,String> entry:map.entrySet()){
+                System.out.println(entry.getKey());
+                System.out.println(entry.getValue());
             }
+//            for (String key : map.keySet()) {
+//
+//            }
         }
     }
 
