2e56765cc9c0a17ea83188b38b64314b0c1be792
Yishan Xu <yxu.dev@gmail.com>
2019-12-05 15:07:03 -0500

    add+

--- src/main/java/com/yishan/oop/TryDateApi.java
+++ src/main/java/com/yishan/oop/TryDateApi.java
@@ -1,14 +1,35 @@
 package com.yishan.oop;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class TryDateApi{
 
-    public static void main(String[] args) {
-        System.out.println(LocalDate.now());
-        System.out.println(LocalTime.now());
+   public static void main(String[] args) {
+//        System.out.println(LocalDate.now());
+//        System.out.println(LocalTime.now());
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String dateInString = "01/12/2001";
+
+        try {
+           Date date= formatter.parse(dateInString);
+            System.out.println( converter(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
-
-
+    private static LocalDate converter(Date dateToConvert) {
+        return dateToConvert.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
 }
+
+
+//TODO
+//convert local date to zoned date time
