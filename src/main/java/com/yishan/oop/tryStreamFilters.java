2a6f65b86f42f490f263048f390222e8e8da9379
Yishan Xu <yxu.dev@gmail.com>
2019-12-11 17:04:01 -0500

    stream practice // filter()// list convert 2 map

--- src/main/java/com/yishan/oop/tryStreamFilters.java
+++ src/main/java/com/yishan/oop/tryStreamFilters.java
@@ -2,20 +2,25 @@ package com.yishan.oop;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class tryStreamFilters {
 
     public static void main(String[] args) {
 
         List<Integer> list = new ArrayList<>();
-    for(int i=0; i<101;i++){
+        for(int i=0; i<101;i++){
         list.add(i);
+        }
+
+
+        Map<Integer, Boolean> result = list.stream().filter(num -> num%2!=0 && num%3!=0 && num%5!=0 && num%7!=0)
+                .collect(Collectors.toMap(num->num ,num->true));
+        System.out.println(result);
     }
 
-        list.stream().filter(num -> num % 2 == 0 || num %3 == 0 || num%5 == 0).forEach(System.out::println);
 
-
-    }
 
 
 
